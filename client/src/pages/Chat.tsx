cef975834ae62b2e3701bea088575ed981b7eb95
Conny1 <joelconrad277@gmail.com>
2024-01-04 12:13:43 +0300

    chatui

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { styled } from "styled-components";
+import Navbar from "../components/Navbar";
+import autimage from "../images/auth.jpg";
+
+const Maincontainer = styled.div`
+  background-image: url(${autimage});
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh;
+`;
+
+const Container = styled.div`
+  background-image: url(${autimage});
+  background-position: center;
+  background-size: cover;
+  min-height: 100vh;
+`;
+const NavContainer = styled.div`
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <Maincontainer>
+      <NavContainer>
+        <Navbar />
+      </NavContainer>
+      <Container></Container>
+    </Maincontainer>
+  );
 };
 
 export default Chat;
