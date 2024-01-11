c55a430bc2ee44d9741c2d70729ed6ef7af253b9
Conny1 <joelconrad277@gmail.com>
2024-01-11 11:32:00 +0300

    jwtDone

--- api/Routes/chats.js
+++ api/Routes/chats.js
@@ -8,16 +8,21 @@ import {
   removeFromgroup,
   renamegroup,
 } from "../Controlers/Chats.js";
+import {
+  VerifyTokens,
+  verifgroupAdmin,
+  verifyUser,
+} from "../utils/VerifyTokens.js";
 
 const Router = express.Router();
 
-Router.get("/getallusers", getAllusers);
-Router.post("/accesschats", AccessChats);
+Router.get("/getallusers", VerifyTokens, getAllusers);
+Router.post("/accesschats", VerifyTokens, AccessChats);
 
-Router.get("/fetchchats/:id", fetchChats);
-Router.post("/creategroup", creategroup);
-Router.put("/renamegroup/:id", renamegroup);
-Router.put("/adduserstogroup/:id", addgroup);
-Router.put("/removeuserfromgroup/:id", removeFromgroup);
+Router.get("/fetchchats/:id", verifyUser, fetchChats);
+Router.post("/creategroup", VerifyTokens, creategroup);
+Router.put("/renamegroup/:id", verifgroupAdmin, renamegroup);
+Router.put("/adduserstogroup/:id", verifgroupAdmin, addgroup);
+Router.put("/removeuserfromgroup/:id", verifgroupAdmin, removeFromgroup);
 
 export default Router;
